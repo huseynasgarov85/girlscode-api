21151896ea7de1ef3205c3e827124ebdc15eab91
huseynasgarov85 <huseynnaskerovv@gmail.com>
2025-04-09 13:58:48 +0400

    first commit

---
+++ src/main/java/com/example/girlscodeapi/GirlsCodeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.girlscodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirlscodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GirlscodeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/girlscodeapi/GirlsCodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.girlscodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GirlscodeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
