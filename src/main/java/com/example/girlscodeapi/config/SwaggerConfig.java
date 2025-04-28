a23dfcdbd2fdcd1c6c6a4f4668e67f65a833c04e
huseynasgarov85 <huseynnaskerovv@gmail.com>
2025-04-28 16:42:30 +0400

    aaaa

--- src/main/java/com/example/girlscodeapi/config/SwaggerConfig.java
+++ src/main/java/com/example/girlscodeapi/config/SwaggerConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,8 +18,21 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "API Documentation",
+                "API documentation for the deployed server",
+                "1.0",
+                "Terms of service",
+                new Contact("Support", "www.example.com", "support@example.com"),
+                "License",
+                "License URL",
+                Collections.emptyList());
     }
 }
