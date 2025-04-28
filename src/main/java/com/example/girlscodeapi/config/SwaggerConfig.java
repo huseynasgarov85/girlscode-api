97de322db46ac5678bfd250011c305ec2658c7f9
huseynasgarov85 <huseynnaskerovv@gmail.com>
2025-04-28 16:15:28 +0400

    afg

--- src/main/java/com/example/girlscodeapi/config/SwaggerConfig.java
+++ src/main/java/com/example/girlscodeapi/config/SwaggerConfig.java
@@ -1,16 +1,21 @@
 package com.example.girlscodeapi.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.models.info.Info;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("My API").version("1.0").description("Swagger documentation for my API"));
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+                .paths(PathSelectors.any())
+                .build();
     }
 }
