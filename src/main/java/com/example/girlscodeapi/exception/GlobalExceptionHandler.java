afbfbefaeeffc1fc00563efe7c1107425cc8797f
huseynasgarov85 <huseynnaskerovv@gmail.com>
2025-04-29 00:24:49 +0400

    as

--- src/main/java/com/example/girlscodeapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/girlscodeapi/exception/GlobalExceptionHandler.java
@@ -1,18 +1,18 @@
-package com.example.girlscodeapi.exception;
-
-import com.example.girlscodeapi.model.base.BaseResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-@Slf4j
-@RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
-    @ExceptionHandler(BaseException.class)
-    public ResponseEntity<BaseResponse<?>> handleBaseException(BaseException ex) {
-        return ResponseEntity.status(ex.getResponseMessage().status()).body(BaseResponse.error(ex));
-    }
-}
+//package com.example.girlscodeapi.exception;
+//
+//import com.example.girlscodeapi.model.base.BaseResponse;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+//
+//@Slf4j
+//@RestControllerAdvice
+//public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+//
+//    @ExceptionHandler(BaseException.class)
+//    public ResponseEntity<BaseResponse<?>> handleBaseException(BaseException ex) {
+//        return ResponseEntity.status(ex.getResponseMessage().status()).body(BaseResponse.error(ex));
+//    }
+//}
