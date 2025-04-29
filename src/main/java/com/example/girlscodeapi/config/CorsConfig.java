6ced596bed7fe9b22cef78dcf5558518a01b1f2c
huseynasgarov85 <huseynnaskerovv@gmail.com>
2025-04-29 23:23:11 +0400

    as

--- src/main/java/com/example/girlscodeapi/config/CorsConfig.java
+++ src/main/java/com/example/girlscodeapi/config/CorsConfig.java
@@ -3,19 +3,22 @@ package com.example.girlscodeapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*") // ya da frontend URL
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-            }
-        };
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("https://girlscode-api.onrender.com/swagger-ui/index.html")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
-}
+};
+
