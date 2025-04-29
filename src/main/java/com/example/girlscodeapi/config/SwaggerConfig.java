1a8ccaa7cf23a6cd06a4918287b4b625690c14a6
huseynasgarov85 <huseynnaskerovv@gmail.com>
2025-04-29 15:48:34 +0400

    as

--- src/main/java/com/example/girlscodeapi/config/SwaggerConfig.java
+++ src/main/java/com/example/girlscodeapi/config/SwaggerConfig.java
@@ -1,38 +1,19 @@
-//package com.example.girlscodeapi.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.service.Contact;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//
-//import java.util.Collections;
-//
-//@Configuration
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
-//                .paths(PathSelectors.any())
-//                .build()
-//                .apiInfo(apiInfo());
-//    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfo(
-//                "API Documentation",
-//                "API documentation for the deployed server",
-//                "1.0",
-//                "Terms of service",
-//                new Contact("Support", "www.example.com", "support@example.com"),
-//                "License",
-//                "License URL",
-//                Collections.emptyList());
-//    }
-//}
+package com.example.girlscodeapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("GirlsCode API")
+                        .version("1.0")
+                        .description("This is the GirlsCode API documentation."));
+    }
+}
