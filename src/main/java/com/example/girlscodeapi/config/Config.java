7649289b18e4a0507f8c0b3f7973f9180e7f41a4
huseynasgarov85 <huseynnaskerovv@gmail.com>
2025-04-30 13:06:23 +0400

    as

---
+++ src/main/java/com/example/girlscodeapi/config/Config.java
@@ -0,0 +1,20 @@
+package com.example.girlscodeapi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "GirlsCode API",
+                version = "v1",
+                description = "Backend API documentation for GirlsCode Project"
+        ),
+        servers = {
+                @Server(url = "https://girlscode-api.onrender.com", description = "Render Production Server"),
+                @Server(url = "http://localhost:8080", description = "Local Development Server")
+        }
+)
+public class Config {
+    // No need to add anything here
+}
