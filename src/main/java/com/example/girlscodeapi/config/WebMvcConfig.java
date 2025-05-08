8e2deb51c097308de792b45396daa96fe2348577
huseynasgarov85 <huseynnaskerovv@gmail.com>
2025-05-08 19:48:49 +0400

    asd

--- src/main/java/com/example/girlscodeapi/config/WebMvcConfig.java
+++ src/main/java/com/example/girlscodeapi/config/WebMvcConfig.java
@@ -9,18 +9,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/")
-                        .allowedOrigins("*")
-                        .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
-                        .allowedHeaders("*")
-                        .exposedHeaders("*");
-                //.allowCredentials(true);
-            }
-        };
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(@NonNull CorsRegistry registry) {
+//                registry.addMapping("/")
+//                        .allowedOrigins("*")
+//                        .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+//                        .allowedHeaders("*")
+//                        .exposedHeaders("*");
+//                //.allowCredentials(true);
+//            }
+//        };
+//    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/")
+                .allowedOrigins("https://girlscode-api-1.onrender.com", "http://localhost:*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
