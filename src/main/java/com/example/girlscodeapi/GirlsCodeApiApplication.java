68bf19361c857f2c6120da4105a047f67603c5a7
huseynasgarov85 <huseynnaskerovv@gmail.com>
2025-05-10 02:53:07 +0400

    Add credential.json file

--- src/main/java/com/example/girlscodeapi/GirlsCodeApiApplication.java
+++ src/main/java/com/example/girlscodeapi/GirlsCodeApiApplication.java
@@ -1,7 +1,13 @@
 package com.example.girlscodeapi;
 
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @SpringBootApplication
 public class GirlsCodeApiApplication {
@@ -14,6 +20,18 @@ public class GirlsCodeApiApplication {
         if (credentialsPath == null) {
             System.out.println("Ətraf mühit dəyişəni tanınmadı.");
             return;
+        }
+
+
+        try {
+            Storage storage = StorageOptions.newBuilder()
+                    .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream("/etc/secrets/gcp-credentials.json")))
+                    .build()
+                    .getService();
+
+            System.out.println("Google Cloud Storage connection successful!");
+        } catch (IOException e) {
+            System.err.println("Failed to authenticate with Google Cloud: " + e.getMessage());
         }
 
     }
